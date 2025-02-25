f7b348e312f8ee92668836378746d864e7a972e8
BrianHammond <brianearlhammond@gmail.com>
2025-02-25 20:44:36 +0900

    support for FastAPI

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ I'm still learning Python and using these as reference guides for any employment
 
 This is a simple UI client that supports RESTful Post, Get, Put and Delete commands.
 
-I'm using my [Flask API Tool ](https://github.com/BrianHammond/Flask_API_Tool) as a test since it's easy to understand.
+I'm using my [FASTapi Tool ](https://https://github.com/BrianHammond/FAST_API_Tool) as a test since it's easy to understand.
 
 Click on run.bat to run the program, the bat file will install pyside6, qdarkstyle and requests if they are not already installed on your system and start the application.
 
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
         self.settings = QSettings('settings.ini', QSettings.IniFormat)
         self.settings_manager = SettingsManager(self)  # Initializes SettingsManager
         self.settings_manager.load_settings()  # Load settings when the app starts
-        self.flask_api = FlaskAPI() # initialize FlaskAPI class
+        self.api = API() # initialize FlaskAPI class
 
         # Connect line_server to the update_base_url method
         self.line_server.returnPressed.connect(self.update_base_url)       
@@ -41,47 +41,57 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
 
         self.initialize_table()
 
+    def employee_data(self, id, first_name, middle_name, last_name, age, title, address1, address2, misc):
+        data = {
+            "employee_id": int(id),
+            "name": {
+                "first_name": first_name,
+                "middle_name": middle_name,
+                "last_name": last_name
+            },
+            "age": int(age),
+            "title": title,
+            "address": {
+                "address_1": address1,
+                "address_2": address2
+            },
+            "misc": misc
+        }
+        return data
+
     def update_base_url(self):
         new_url = f"http://{self.line_server.text()}"
 
-        self.flask_api.base_url = new_url
-        print(f"Flask API base URL updated to: {self.flask_api.base_url}")
+        self.api.base_url = new_url
+        print(f"API base URL updated to: {self.api.base_url}")
 
         # check the connection if base_url is set
-        if self.flask_api.base_url:
-            self.flask_api.is_connected = self.flask_api.check_connection()
+        if self.api.base_url:
+            self.api.is_connected = self.api.check_connection()
             self.update_connection_status()
 
-    def api_post(self):
+    def api_post(self): # add data
         self.current_date = datetime.datetime.now().strftime("%m%d%Y%H%M%S")
         id = self.current_date
         
         # Get the values from the QLineEdits
-        name = self.line_name.text()  # QLineEdit for Name
-        age = self.line_age.text()    # QLineEdit for Age
-        title = self.line_title.text()  # QLineEdit for Title
-        address1 = self.line_address1.text()  # QLineEdit for Address 1
-        address2 = self.line_address2.text()  # QLineEdit for Address 2
-        misc = self.line_misc.text()  # QLineEdit for Misc
+        first_name = self.line_firstname.text()
+        middle_name = self.line_middlename.text()
+        last_name = self.line_lastname.text()
+        age = self.line_age.text()
+        title = self.line_title.text()
+        address1 = self.line_address1.text()
+        address2 = self.line_address2.text()
+        misc = self.line_misc.text()
 
         row = self.table.rowCount()
-        self.populate_table(row, id, name, age, title, address1, address2, misc)
+        self.populate_table(row, id, first_name, middle_name, last_name, age, title, address1, address2, misc)
 
-        # Prepare the data in the format expected by Flask
-        data = {
-            "ID": id,
-            "Name": name,
-            "Age": age,
-            "Title": title,
-            "Address": {
-                "Address 1": address1,
-                "Address 2": address2
-            },
-            "Misc": misc
-        }
+        # Prepare the data in the format required by the API
+        data = self.employee_data(id, first_name, middle_name, last_name, age, title, address1, address2, misc)
 
         # Send the data using FlaskAPI's send_post method
-        response = self.flask_api.send_post(data)
+        response = self.api.send_post(data)
         if response:
             print("Data sent successfully:", response)
         else:
@@ -89,18 +99,17 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
 
         self.clear_fields()
 
-    def api_get(self):
-        print("get pressed")
+    def api_get(self): # get data
         
         # Fetch the data from the Flask API using the send_get method
-        data = self.flask_api.send_get()
+        data = self.api.send_get()
         
         if data:
             print("Data received from API:", data)  # Log the received data
 
             # Check if the data contains the 'Employees' key
-            if "Employees" in data:
-                employees = data["Employees"]  # Get the list of employees
+            if "employees" in data:
+                employees = data["employees"]  # Get the list of employees
                 
                 # Initialize the table to ensure it is cleared before populating new data
                 self.initialize_table()
@@ -108,26 +117,27 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
                 for record in employees:
                     if isinstance(record, dict):
                         # Extract values for each record
-                        id = record.get("ID")
-                        name = record.get("Name")
-                        age = record.get("Age")
-                        title = record.get("Title")
-                        address1 = record.get("Address", {}).get("Address 1", "")
-                        address2 = record.get("Address", {}).get("Address 2", "")
-                        misc = record.get("Misc")
+                        id = record.get("employee_id")
+                        first_name = record.get("name", {}).get("first_name", "")
+                        middle_name = record.get("name", {}).get("middle_name", "")
+                        last_name = record.get("name", {}).get("last_name", "")
+                        age = record.get("age")
+                        title = record.get("title")
+                        address1 = record.get("address", {}).get("address_1", "")
+                        address2 = record.get("address", {}).get("address_2", "")
+                        misc = record.get("misc")
 
                         # Find the next available row in the table
                         row = self.table.rowCount()
 
                         # Populate the table with the values
-                        self.populate_table(row, id, name, age, title, address1, address2, misc)
+                        self.populate_table(row, id, first_name, middle_name, last_name, age, title, address1, address2, misc)
             else:
                 print("Unexpected data format: Missing 'Employees' key")
         else:
             print("Failed to retrieve data.")
 
-    def api_put(self):
-        # Get the selected row from the table
+    def api_put(self): # update data
         selected_row = self.table.currentRow()
         
         if selected_row == -1:  # No row selected
@@ -136,28 +146,26 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
 
         # Extract updated data from the table's cells
         id = self.table.item(selected_row, 0).text()
-        name = self.table.item(selected_row, 1).text()
-        age = self.table.item(selected_row, 2).text()
-        title = self.table.item(selected_row, 3).text()
-        address1 = self.table.item(selected_row, 4).text()
-        address2 = self.table.item(selected_row, 5).text()
-        misc = self.table.item(selected_row, 6).text()
+        first_name = self.table.item(selected_row, 1).text()
+        middle_name = self.table.item(selected_row, 2).text()
+        last_name = self.table.item(selected_row, 3).text()
+        age = self.table.item(selected_row, 4).text()
+        title = self.table.item(selected_row, 5).text()
+        address1 = self.table.item(selected_row, 6).text()
+        address2 = self.table.item(selected_row, 7).text()
+        misc = self.table.item(selected_row, 8).text()
 
         # Prepare the data to be sent in the PUT request
-        data = {
-            "ID": id,
-            "Name": name,
-            "Age": age,
-            "Title": title,
-            "Address": {
-                "Address 1": address1,
-                "Address 2": address2
-            },
-            "Misc": misc
-        }
+        data = self.employee_data(id, first_name, middle_name, last_name, age, title, address1, address2, misc)
 
-        # Send the updated data using FlaskAPI's send_put method
-        response = self.flask_api.send_put(data)
+        # Debugging output
+        print(f"Data to be sent in PUT request: {data}")
+
+        response = self.api.send_put(data)
+        
+        # Debugging response
+        print(f"Response from PUT request: {response}")
+
         if response:
             print("Data updated successfully:", response)
             QMessageBox.information(self, "Success", "Employee data updated successfully.")
@@ -165,9 +173,7 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
             print("Failed to update data.")
             QMessageBox.warning(self, "Error", "Failed to update employee data.")
 
-        self.table.resizeColumnsToContents()
-
-    def api_delete(self):
+    def api_delete(self): # delete data
         # Get the selected rows from the table
         selected_rows = self.table.selectedIndexes()
 
@@ -189,7 +195,7 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
                 employee_id = self.table.item(row, 0).text()  # Extract the ID of the employee
 
                 # Send DELETE request using FlaskAPI's send_delete method
-                response = self.flask_api.send_delete(employee_id)
+                response = self.api.send_delete(employee_id)
 
                 if response:
                     print(f"Employee with ID {employee_id} deleted successfully:", response)
@@ -200,24 +206,28 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
 
     def initialize_table(self):
         self.table.setRowCount(0) # clears the table
-        self.table.setColumnCount(7)
-        self.table.setHorizontalHeaderLabels(['ID', 'Name', 'Age', 'Title', 'Address 1', 'Address 2', 'Misc'])
+        self.table.setColumnCount(9)
+        self.table.setHorizontalHeaderLabels(['ID', 'First Name', 'Middle Name', 'Last Name', 'Age', 'Title', 'Address 1', 'Address 2', 'Misc'])
         self.table.setSelectionMode(QTableWidget.MultiSelection)
 
-    def populate_table(self, row, id, name, age, title, address1, address2, misc):
+    def populate_table(self, row, id, first_name, middle_name, last_name, age, title, address1, address2, misc):
         self.table.insertRow(row)
         self.table.setItem(row, 0, QTableWidgetItem(str(id)))
-        self.table.setItem(row, 1, QTableWidgetItem(name))
-        self.table.setItem(row, 2, QTableWidgetItem(age))
-        self.table.setItem(row, 3, QTableWidgetItem(title))
-        self.table.setItem(row, 4, QTableWidgetItem(address1))
-        self.table.setItem(row, 5, QTableWidgetItem(address2))
-        self.table.setItem(row, 6, QTableWidgetItem(misc))
+        self.table.setItem(row, 1, QTableWidgetItem(first_name))
+        self.table.setItem(row, 2, QTableWidgetItem(middle_name))
+        self.table.setItem(row, 3, QTableWidgetItem(last_name))
+        self.table.setItem(row, 4, QTableWidgetItem(str(age)))
+        self.table.setItem(row, 5, QTableWidgetItem(title))
+        self.table.setItem(row, 6, QTableWidgetItem(address1))
+        self.table.setItem(row, 7, QTableWidgetItem(address2))
+        self.table.setItem(row, 8, QTableWidgetItem(misc))
         self.table.resizeColumnsToContents()
         self.table.resizeRowsToContents()
 
     def clear_fields(self):
-        self.line_name.clear()
+        self.line_firstname.clear()
+        self.line_middlename.clear()
+        self.line_lastname.clear()
         self.line_age.clear()
         self.line_title.clear()
         self.line_address1.clear()
@@ -225,8 +235,8 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
         self.line_misc.clear()
 
     def update_connection_status(self):
-        self.flask_api.is_connected = self.flask_api.check_connection()
-        if self.flask_api.is_connected:
+        self.api.is_connected = self.api.check_connection()
+        if self.api.is_connected:
             self.label_connection.setText("Connected to FlaskAPI")
         else:
             self.label_connection.setText("Failed to connect to FlaskAPI")
@@ -248,7 +258,7 @@ class MainWindow(QMainWindow, main_ui): # used to display the main user interfac
         self.settings_manager.save_settings()  # Save settings using the manager
         event.accept()
 
-class FlaskAPI: # Connects to the Flask API
+class API: # Connects to the API
     def __init__(self):
         self.base_url = None
         self.is_connected = False
@@ -266,7 +276,6 @@ class FlaskAPI: # Connects to the Flask API
     
     def send_post(self, data):
         try:
-            # Send a POST request to the Flask API
             response = requests.post(f'{self.base_url}/postdata', json=data)
             
             if response.status_code // 100 == 2:  # Success: checks for 2xx status codes
@@ -281,7 +290,6 @@ class FlaskAPI: # Connects to the Flask API
         
     def send_get(self):
         try:
-            # Send a GET request to the Flask API
             response = requests.get(f'{self.base_url}/getdata')
 
             if response.status_code // 100 == 2:  # Success: checks for 2xx status codes
@@ -304,11 +312,17 @@ class FlaskAPI: # Connects to the Flask API
 
     def send_put(self, data):
         try:
-            # Send PUT request to Flask API
-            response = requests.put(f'{self.base_url}/putdata', json=data)
-            if response.status_code // 100 == 2:
+            url = f'{self.base_url}/putdata/{data["employee_id"]}'  # Use the ID directly in the URL
+            response = requests.put(url, json=data)  # Send the PUT request with the ID in the URL
+            
+            # Debugging output
+            print(f"PUT response status code: {response.status_code}")
+            print(f"PUT response text: {response.text}")
+            
+            if response.status_code // 100 == 2:  # Success: checks for 2xx status codes
                 return response.json()
             else:
+                print(f"PUT request failed with status code: {response.status_code}")
                 return None
         except requests.RequestException as e:
             print(f"PUT request error: {e}")
@@ -316,8 +330,7 @@ class FlaskAPI: # Connects to the Flask API
 
     def send_delete(self, employee_id):
         try:
-            # Send a DELETE request to the Flask API
-            response = requests.delete(f'{self.base_url}/deletedata', json={"ID": employee_id})
+            response = requests.delete(f'{self.base_url}/deletedata/{employee_id}')
 
             if response.status_code // 100 == 2:  # Success: checks for 2xx status codes
                 print("DELETE request successful:", response.json())
